f7d5b3f137a21a48d4bb31a5f777a0e9fb106db1
khushaalsharma <khushaalsharma1@gmail.com>
2026-05-29 00:41:06 +0530

    added caching in config and validation in DTOs

--- app/src/main/java/com/rate_limiter/app/controller/RateLimitController.java
+++ app/src/main/java/com/rate_limiter/app/controller/RateLimitController.java
@@ -2,6 +2,8 @@ package com.rate_limiter.app.controller;
 
 import com.rate_limiter.app.service.RateLimiterService;
 import com.rate_limiter.app.service.algorithms.RateLimitResult;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.rate_limiter.app.DTO.RateLimitRequest;
 
 import java.util.Map;
 
@@ -19,9 +22,9 @@ public class RateLimitController {
     private final RateLimiterService rateLimiterService;
 
     @PostMapping("/check")
-    public ResponseEntity<Map<String, Object>> checkRateLimit(@RequestBody Map<String, Object> request) throws Exception {
-        String clientKey = request.get("clientKey").toString();
-        String endpoint = request.getOrDefault("endpoint", "unknown").toString();
+    public ResponseEntity<Map<String, Object>> checkRateLimit(@Valid @RequestBody RateLimitRequest request) throws Exception {
+        String clientKey = request.getClientKey();
+        String endpoint = request.getEndpoint() != null ? request.getEndpoint() : "unknown" ;
 
         RateLimitResult result = rateLimiterService.checkRateLimit(clientKey, endpoint);
 
